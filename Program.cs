15bcda040483162e3235c72ce96d3b473cdbb692
jorgenmjobloop <jorgen@jobloop.no>
2024-05-28 11:30:40 +0200

    added Quicksort() method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
-﻿namespace KH_CS_Uke4;
+﻿using System.Diagnostics;
+
+namespace KH_CS_Uke4;
 class Program
 {
     static void Main(string[] args)
     {
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+        
+        
+
         Algorithms algorithms = new Algorithms();
         int[] sampleArray = {1,10,9,82, 24, 28, 16, 1028, 512, 256};
         algorithms.BubbleSort(sampleArray);
         Console.WriteLine("Bubble sort output: ");
-        Console.WriteLine("[{0}]", string.Join(" ", sampleArray));
+        Console.WriteLine(string.Join(" ", sampleArray));
+
+        stopwatch.Stop();
+        Console.WriteLine($"Bubblesort ran for: {stopwatch.ElapsedMilliseconds} milliseconds");
+
+        stopwatch.Start();
+
+        int[] secondSample = {25, 150, 250, 10, 82, 7, 9, 3, 1};
+        algorithms.Quicksort(secondSample, 0, secondSample.Length - 1);
+        Console.WriteLine("Sorted array using Quicksort: ");
+        Console.WriteLine(string.Join(" ", secondSample));
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Quicksort ran for: {stopwatch.ElapsedMilliseconds} milliseconds");
+
     }
 }
