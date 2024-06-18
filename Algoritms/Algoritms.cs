87a2da29f3ef73b6fb6f16d37d03fb01b3716fd6
jorgenmjobloop <jorgen@jobloop.no>
2024-06-18 09:57:12 +0200

    Made changes to algorithms & program.cs

--- Algoritms/Algoritms.cs
+++ Algoritms/Algoritms.cs
@@ -57,7 +57,7 @@ public class Algorithms
     private int Recursion(int[] arr, int low, int high)
     {
         int pivot = arr[high];
-        int i = (low - 1);
+        int i = low - 1;
         for(int j = low; j < high; j++) 
         {
             if(arr[j] < pivot) 
